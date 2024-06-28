41cbe5eb31b159aa24bbe4c7e51bc04f5256c728
Flavius <flaviuscdinu93@gmail.com>
2024-06-28 12:43:16 +0300

    Added spacelift configuration

--- tofu/ec2/main.tf
+++ tofu/ec2/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "this" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
